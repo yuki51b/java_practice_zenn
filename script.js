d37596c3be746fd9413cb3ea284662d181084100
yukimura51b <yukimura.3720@gmail.com>
2024-10-17 20:41:29 +0900

    02_chapter

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-document.querySelector('p').textContent = 'Hello World!!';
-//window.alert('Hello World!!');
-console.log('Hello World');
+'use strict';
+
+const weight = 65;
+const height = 1.70;
+console.log('体重' + weight + 'kg/身長' + (height * 100) + 'cmのBMI');
+console.log(weight / (height * height));
+
+let who = 'World';
+console.log('Hello' + who);
+who = "everyone";
+console.log('Good Morning!' + who);
